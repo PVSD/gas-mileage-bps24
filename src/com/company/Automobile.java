2a0ff520aa0038898a5cc924ab79f710883fc6a5
bs482 <bs482@pvsd.org>
2018-10-12 10:39:58 -0400

    finsihed haha

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,24 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+
+    public int milesPerGallon;
+    public double gallons=0.0;
+
+    public Automobile (int a)
+    {
+        milesPerGallon = a;
+    }
+    public void fillUp (int b)
+    {
+        gallons =+ b;
+    }
+    public void takeTrip (int c)
+    {
+        gallons = gallons - c/milesPerGallon;
+    }
+    public double reportFuel ()
+    {
+        return gallons;
+    }
 }
